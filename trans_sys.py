17d3b98a0b8cf8a39ce727a21a6e54c13bcf3407
f30d0r0v <69794505+f30d0r0v@users.noreply.github.com>
2022-06-08 23:26:20 +0300

    Update trans_sys.py

--- trans_sys.py
+++ trans_sys.py
@@ -1,30 +1,32 @@
 x=int(input('Введите основание системы... ', ))
 v=int(input('Число в десятичной системе = ', ))
+temp = ""
 while v>0:
     y=v%x
     if y==10:
-        print('A')
+        temp+='A'
     elif y==11:
-        print('B')
+        temp+='B'
     elif y==12:
-        print("C")
+        temp+="C"
     elif y==13:
-        print("D")
+        temp+="D"
     elif y==14:
-        print("E")
+        temp+="E"
     elif y==15:
-        print("F")
+        temp+="F"
     elif y==16:
-        print("G")
+        temp+="G"
     elif y==17:
-        print("H")
+        temp+="H"
     elif y==18:
-        print("I")
+        temp+="I"
     elif y==19:
-        print("J")
+        temp+="J"
     elif y==20:
-        print("K")
+        temp+="K"
     else:
-        print(y)
+        temp+=str(y)
     v=v//x
-print('Ответ читать с конца! Если основание системы больше, чем 21, то в тех местах, где будут дву- или более значные числа,пишите заглавные латинские буквы: А=10...К=20, L=21...')
+print(temp[::-1])
+
